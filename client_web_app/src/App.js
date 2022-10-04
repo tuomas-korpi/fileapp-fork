56a0a2817af3c789c088b8c77b441fc2f70e9dfc
Tuomas Korpi <tuomas.korpi@nordcloud.com>
2022-10-04 18:51:25 +0300

    sign out

--- client_web_app/src/App.js
+++ client_web_app/src/App.js
@@ -6,11 +6,48 @@ import axios from 'axios';
 import Create from "./components/create"
 import blobs from "./components/blobs"
 import { PageLayout } from "./components/PageLayout";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal  } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import Button from "react-bootstrap/Button";
 
 //change proxy too
 const baseUrl = "http://localhost:3001"
 //const baseUrl = "https://api321tuomas.azurewebsites.net"
 
+
+function ProfileContent() {
+  const { instance, accounts, inProgress } = useMsal();
+  const [accessToken, setAccessToken] = useState(null);
+
+  const name = accounts[0] && accounts[0].name;
+
+  function RequestAccessToken() {
+      const request = {
+          ...loginRequest,
+          account: accounts[0]
+      };
+
+      // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+      instance.acquireTokenSilent(request).then((response) => {
+          setAccessToken(response.accessToken);
+      }).catch((e) => {
+          instance.acquireTokenPopup(request).then((response) => {
+              setAccessToken(response.accessToken);
+          });
+      });
+  }
+
+  return (
+      <>
+          <h5 className="card-title">Welcome {name}</h5>
+          {accessToken ? 
+              <p>Access Token Acquired!</p>
+              :
+              <Button variant="secondary" onClick={RequestAccessToken}>Request Access Token</Button>
+          }
+      </>
+  );
+};
 
 
 
@@ -18,6 +55,8 @@ function App() {
   const [container, setContainers] = useState([]);
   const [blob, setBlob] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  
 
 
   //GET
@@ -49,47 +88,53 @@ function App() {
 
   return (
     <PageLayout>
-    <div className="App">
-      <h1>My Files</h1>
-      {loading ? (
-        <div>...Data Loading.....</div>
-      ) : (
-      <table>
-        <tbody>
-          <tr>
-            <th>File name</th>
-            <th>File url</th>
-            <th>last modified</th>
-            <th>delete</th>
-          </tr>
-          {blob.map(x =>
-            <tr key={Math.random() * 9999}>
-              <td key={Math.random() * 9999}>
-                {x.file_name}
-              </td>
-              <td key={Math.random() * 9999}>
-                {x.url}
-              </td>
-              <td key={Math.random() * 9999}>
-                {x.lastmodified}
-              </td>
-              <td key={Math.random() * 9999}>
-                <button onClick={event => handleDelete(event, x.id)}>
-                  delete
-                </button>
-              </td>
-            </tr>
-          )
-          }
-        </tbody>
-      </table>)}
+      <AuthenticatedTemplate>
+      <ProfileContent />
+        <div className="App">
+          <h1>My Files</h1>
+          {loading ? (
+            <div>...Data Loading.....</div>
+          ) : (
+            <table>
+              <tbody>
+                <tr>
+                  <th>File name</th>
+                  <th>File url</th>
+                  <th>last modified</th>
+                  <th>delete</th>
+                </tr>
+                {blob.map(x =>
+                  <tr key={Math.random() * 9999}>
+                    <td key={Math.random() * 9999}>
+                      {x.file_name}
+                    </td>
+                    <td key={Math.random() * 9999}>
+                      {x.url}
+                    </td>
+                    <td key={Math.random() * 9999}>
+                      {x.lastmodified}
+                    </td>
+                    <td key={Math.random() * 9999}>
+                      <button onClick={event => handleDelete(event, x.id)}>
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+                }
+              </tbody>
+            </table>)}
 
 
-      <hr />
-      <div>
-        <Create />
-      </div>
-    </div>
+          <hr />
+          <div>
+            <Create />
+          </div>
+        </div>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <p>You are not signed in! Please sign in.</p>
+      </UnauthenticatedTemplate>
     </PageLayout>
   );
 }
