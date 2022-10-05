41e659385683d16aac453d1bb61e49c6b89a0cc1
Xuefeng Luo <hakunamatatahenry@gmail.com>
2022-10-05 14:21:41 +0200

    write file meta into db

--- server_web_app/blobStorage.js
+++ server_web_app/blobStorage.js
@@ -1,6 +1,6 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
-require('dotenv').config() 
+require('dotenv').config()
 const account = process.env.STOR_ACCOUNT;
 const accountKey = process.env.SHARED_KEY;
 const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
@@ -29,24 +29,24 @@ const getContainerList = async function () {
 const getBlobList = async function (containerName) {
 
 
-    console.log("\nListing blobs...");
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-    //blob properties
-    blobArr = []
-    // List the blob(s) in the container.
-    for await (const blob of containerClient.listBlobsFlat()) {
-      // Get Blob Client from name, to get the URL
-      const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
-        //put to obj
+  console.log("\nListing blobs...");
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  //blob properties
+  blobArr = []
+  // List the blob(s) in the container.
+  for await (const blob of containerClient.listBlobsFlat()) {
+    // Get Blob Client from name, to get the URL
+    const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
+    //put to obj
         let blobObj = {name:"",url:""}
-        blobObj.name = blob.name
+    blobObj.name = blob.name
         blobObj.url= tempBlockBlobClient.url
-        blobArr.push(blobObj)
-      // Display blob name and URL
-      //console.log(`\n\tname: ${blob.name}\n\tURL: ${tempBlockBlobClient.url}\n`);
-    }
-    // return array of objects
-    return await blobArr
+    blobArr.push(blobObj)
+    // Display blob name and URL
+    //console.log(`\n\tname: ${blob.name}\n\tURL: ${tempBlockBlobClient.url}\n`);
+  }
+  // return array of objects
+  return await blobArr
 }
 
 
@@ -54,19 +54,28 @@ const getBlobList = async function (containerName) {
 const uploadBlob = async function(blobFile, loacalAccountId) {
 
   try{
-  const containerName = "class1";
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blobName = blobFile.originalname
-  console.log(blobFile.originalname);
-  const content = blobFile.buffer
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
-  //url construction https://storcafla426wsqmw.blob.core.windows.net/class1/azuresql.png
+    const containerName = "class1";
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobName = blobFile.originalname
+    console.log(blobFile.originalname);
+    const content = blobFile.buffer
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
+    //url construction https://storcafla426wsqmw.blob.core.windows.net/class1/azuresql.png
     const blobUrl = `https://storcafla426wsqmw.blob.core.windows.net/${containerName}/${blobName}`
     console.log(blobUrl);
-  console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse);
-  //const msg = `Upload block blob ${blobName} successfully`;
-  return uploadBlobResponse
+    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse);
+    //const msg = `Upload block blob ${blobName} successfully`;
+
+    // write into database
+    dbInsertTest(blobName, loacalAccountId, blobUrl).then(() => {
+      res.send("Successfully upload file!");
+    }).catch(err => {
+      console.error(err);
+      throw err;
+    });
+
+    return uploadBlobResponse
   }catch(err){
     console.log("Error uplosd");
   }
